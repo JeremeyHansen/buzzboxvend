c7a41f361a003ec0ba24033bce309856013bd3ed
Jeremey Hansen <jeremeyhansen@yahoo.com>
2024-03-27 12:33:59 -0700

    fixed nav to close on click

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,49 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Navigation = (props) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsMenuOpen(false); // Close the menu when a link is clicked
+  };
+
   return (
-    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+    <nav id="menu" className={`navbar navbar-default navbar-fixed-top ${isMenuOpen ? 'open' : ''}`}>
       <div className="container">
         <div className="navbar-header">
           <button
             type="button"
             className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
+            onClick={handleMenuToggle}
           >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
             Buzz <span className="nav-title1"> Box</span>.
           </a>{" "}
         </div>
 
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
+        <div className={`collapse navbar-collapse ${isMenuOpen ? 'in' : ''}`}>
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#about" className="page-scroll">
+              <a href="#about" className="page-scroll" onClick={handleLinkClick}>
                 About
               </a>
             </li>
             <li>
-              <a href="#services" className="page-scroll">
+              <a href="#services" className="page-scroll" onClick={handleLinkClick}>
                 Services
               </a>
             </li>
             <li>
-              <a href="#team" className="page-scroll">
+              <a href="#team" className="page-scroll" onClick={handleLinkClick}>
                 Team
               </a>
             </li>
             <li>
-              <a href="#contact" className="page-scroll">
+              <a href="#contact" className="page-scroll" onClick={handleLinkClick}>
                 Contact
               </a>
             </li>
